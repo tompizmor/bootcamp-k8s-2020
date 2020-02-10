155dd8d3ba96785d035dcf549a57f5728a2faff1
tompizmor <tompizmor@gmail.com>
2020-02-10 16:55:06 +0100

    Add more examples

--- guestbook/command.bash
+++ guestbook/command.bash
@@ -2,7 +2,7 @@
 
 ## Create frontend deployment
 kubectl create -f part-1/frontend-deployment.yaml
-## Get Pcdod Name & Port-Forwarding
+## Get Pod Name & Port-Forwarding
 kubectl port-forward $(kubectl get pods -l app=guestbook,tier=frontend -o jsonpath="{.items[0].metadata.name}") 3000:3000 &
 curl 127.0.0.1:3000
 ## Acces via browser to 127.0.0.1:3000
@@ -11,8 +11,7 @@ curl 127.0.0.1:3000
 kubectl create -f part-1/redis-master-deployment.yaml
 kubectl create -f part-1/redis-slave-deployment.yaml
 
-
-
+# -------------------------------------------------------------
 
 # Part II
 
